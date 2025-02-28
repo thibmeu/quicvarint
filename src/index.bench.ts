2a65958cbec713a7deb366ca5622bc68dfa45e00
Thibault Meunier <mail@thibaultmeunier.com>
2025-02-28 20:59:30 +0100

    Adding github actions
    Build, test, and compare bench against main

--- src/index.bench.ts
+++ src/index.bench.ts
@@ -10,105 +10,52 @@ const MAX_VARINT_2 = 0x3fff;
 const MAX_VARINT_4 = 0x3fffffff;
 const MAX_VARINT_8 = 0x7fffffff;
 
+const tests = [
+  { name: '1 byte', input: MAX_VARINT_1 },
+  { name: '2 byte', input: MAX_VARINT_2 },
+  { name: '4 byte', input: MAX_VARINT_4 },
+  { name: '8 byte', input: MAX_VARINT_8 },
+];
+
 const randomValues = (num: number, maxValue: number) => {
-    const values = new Array<{v: number, b: Uint8Array}>();
-    for (let i = 0; i < num; i++) {
-        const v = Math.floor(Math.random() * maxValue);
-        values.push({ v, b: encode(v) });
-    }
-    return values;
+  const values = new Array<{ v: number, b: Uint8Array }>();
+  for (let i = 0; i < num; i++) {
+    const v = Math.floor(Math.random() * maxValue);
+    values.push({ v, b: encode(v) });
+  }
+  return values;
 };
 
 describe('Benchmarking read', () => {
-    bench('1-byte', () => {
-        const inputs = randomValues(1024, MAX_VARINT_1);
+  tests.forEach(({ name, input }) => {
+      bench(name, () => {
+        const inputs = randomValues(1024, input);
         for (const input of inputs) {
-            const dataView = new DataView(input.b.buffer);
-            read(dataView, 0);
+          const dataView = new DataView(input.b.buffer);
+          read(dataView, 0);
         }
-    });
-
-    bench('2-byte', () => {
-        const inputs = randomValues(1024, MAX_VARINT_2);
-        for (const input of inputs) {
-            const dataView = new DataView(input.b.buffer);
-            read(dataView, 0);
-        }
-    });
-
-    bench('4-byte', () => {
-        const inputs = randomValues(1024, MAX_VARINT_4);
-        for (const input of inputs) {
-            const dataView = new DataView(input.b.buffer);
-            read(dataView, 0);
-        }
-    });
-
-    bench('8-byte', () => {
-        const inputs = randomValues(1024, MAX_VARINT_8);
-        for (const input of inputs) {
-            const dataView = new DataView(input.b.buffer);
-            read(dataView, 0);
-        }
-    });
+      });
+  })
 });
 
 describe('Benchmarking encode', () => {
-  bench('1-byte', () => {
-    const inputs = randomValues(1024, MAX_VARINT_1);
-    for (const input of inputs) {
-        encode(input.v);
-    }
-  });
-
-  bench('2-byte', () => {
-    const inputs = randomValues(1024, MAX_VARINT_2);
-    for (const input of inputs) {
-        encode(input.v);
-    }
-  });
-
-  bench('4-byte', () => {
-    const inputs = randomValues(1024, MAX_VARINT_4);
-    for (const input of inputs) {
-        encode(input.v);
-    }
-  });
-
-  bench('8-byte', () => {
-    const inputs = randomValues(1024, MAX_VARINT_8);
-    for (const input of inputs) {
-        encode(input.v);
-    }
-  });
+  tests.forEach(({ name, input }) => {
+      bench(name, () => {
+        const inputs = randomValues(1024, input);
+        for (const input of inputs) {
+          encode(input.v);
+        }
+      });
+  })
 })
 
 describe('Benchmarking decode', () => {
-  bench('1-byte', () => {
-    const inputs = randomValues(1024, MAX_VARINT_1);
-    for (const input of inputs) {
-      decode(input.b.buffer);
-    }
-  });
-
-  bench('2-byte', () => {
-    const inputs = randomValues(1024, MAX_VARINT_2);
-    for (const input of inputs) {
-      decode(input.b.buffer);
-    }
-  });
-
-  bench('4-byte', () => {
-    const inputs = randomValues(1024, MAX_VARINT_4);
-    for (const input of inputs) {
-      decode(input.b.buffer);
-    }
-  });
-
-  bench('8-byte', () => {
-    const inputs = randomValues(1024, MAX_VARINT_8);
-    for (const input of inputs) {
-      decode(input.b.buffer);
-    }
-  });
+  tests.forEach(({ name, input }) => {
+      bench(name, () => {
+        const inputs = randomValues(1024, input);
+        for (const input of inputs) {
+          decode(input.b.buffer);
+        }
+      });
+  })
 })
